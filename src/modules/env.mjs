24a0cf9284c56d7e99df602cbf4eb4396cbdea75
Abourass <abourassa@assetval.com>
2019-09-10 11:26:19 -0600

    Made it more readable

--- src/modules/env.mjs
+++ src/modules/env.mjs
@@ -1,14 +1,27 @@
-// Shorthand for our log
+/* ==========================================================================
+//                           Environment Functions                         //
+=============================================================================
+                              TABLE OF CONTENTS
+ =============================================================================
+ 0. log - An alias for console.log
+ 1. asyncForEach - Async compatible forEach
+ 2. µ - Grab whatever element on a page you need (Uses querySelector)
+ 3. µAll - Grab All Selectors on the page (Uses querySelectorAll)
+ 4. grabAll - A simplified version of µAll
+ 5. pageDone - executes the function in the callback when the DOM is rendered
+ ===========================================================================*/
+
+// 00 - An alias for console.log
 export const log = console.log;
 
-// Async for each
+// 01 - Async compatible forEach
 export const asyncForEach = async function(array, callback) {
   for (let index = 0; index < array.length; index++) { // eslint-disable-next-line no-await-in-loop
     await callback(array[index], index, array);
   }
 };
 
-// Grab whatever element on a page you need (Uses querySelector)
+// 02 - Grab whatever element on a page you need (Uses querySelector)
 export const µ = function(selector) {
   let el;
   const obj = {
@@ -55,7 +68,7 @@ export const µ = function(selector) {
   return obj;
 };
 
-// Grab All Selectors on the page (Uses querySelectorAll)
+// 03 - Grab All Selectors on the page (Uses querySelectorAll)
 export const µAll = async(selector) => {
   let el;
   const obj = {
@@ -96,17 +109,17 @@ export const µAll = async(selector) => {
   return obj;
 };
 
-// Grabs all instances of a selector on page as an array;
+// 04 - Grabs all instances of a selector on page as an array;
 export const grabAll = selector => document.querySelectorAll(selector);
 
-// Initiates the callback function when the dom is done rendering
+// 05 - Initiates the callback function when the dom is done rendering
 export const pageDone = function(cb){ // Replaces jQuery document ready
   if (document.readyState === 'complete' || (document.readyState !== 'loading' && !document.documentElement.doScroll)) {
     cb();
   } else { document.addEventListener('DOMContentLoaded', cb); }
 };
 
-export default {log, µ, asyncForEach, grabAll, pageDone, µAll};
+export default {log, asyncForEach, µ, µAll, grabAll, pageDone, };
 
 // σ = alt + 741
 // µ = alt + 742
--- src/modules/sweetAlerts.mjs
+++ src/modules/sweetAlerts.mjs
@@ -4,7 +4,7 @@ import {createNewPlayer} from './userFunctions.mjs';
 //                           SweetAlert2 Functions                         //
 ===========================================================================*/
 
-// Get Characters Name (Use's SweetAlert Prompt to Ask For Character Name)
+// Prompt for the players name, and create a new instance of the player class from that name
 export const player = async() => {
   const newPlayer = await Swal.fire({
     title: 'What is your characters name?',
